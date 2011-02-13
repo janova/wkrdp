ca92640efc9c0f5a2f2b246c6dd8ccc7f47326b0
Anthony Carlos <anthony.carlos@janova.us>
2011-02-13 09:25:35 -0500

    Rough implementation of functions. Needs refactoring.

--- lib/janova/wkrdp/app.rb
+++ lib/janova/wkrdp/app.rb
@@ -13,13 +13,35 @@ module Janova
       end
     
       def run
+        ec2 = RightAws::Ec2.new(@access_key_id, @secret_access_key)
         puts 'wkrdp:'
         if @options.list
           puts "Here is a list of worker instances."
+          running_windows_instances = ec2.describe_instances.select {|i| i[:aws_platform] == 'windows' && i[:aws_state] == 'running'}
+          preprod_instances = running_windows_instances.select {|i| i[:aws_groups].include? 'preprod'}
+          prod_instances = running_windows_instances.select {|i| i[:aws_groups].include? 'worker_production'}
+          puts "preprod:"
+          preprod_instances.each {|i| puts "#{i[:aws_instance_id]} #{i[:aws_launch_time]} #{i[:dns_name]}"}
+          puts "production:"
+          prod_instances.each {|i| puts "#{i[:aws_instance_id]} #{i[:aws_launch_time]} #{i[:dns_name]}"}
           exit 0
         end
         if @options.instance_id
+          instance = ec2.describe_instances.select {|i| i[:aws_instance_id] =~ /^#{@options.instance_id}/}
+          if instance.size > 1
+            puts "Ambiguous aws_instance_id. Exiting."
+            exit 1
+          end
+          if instance.size < 1
+            puts "aws_instance_id not found. Exiting."
+            exit 1
+          end
+          instance = instance.first
+          command = "open rdp://administrator:esacegde@#{instance[:dns_name]}"
           puts "Here is the string to connect to your worker."
+          puts command
+          exec(command) if fork == nil
+          Process.wait
           exit 0
         end
       end 
--- lib/wkrdp.rb
+++ lib/wkrdp.rb
@@ -1,3 +1,5 @@
 $LOAD_PATH.push File.join(File.dirname(__FILE__), 'janova', 'wkrdp') 
 require 'app'
 require 'options'
+require 'rubygems'
+require 'right_aws'
