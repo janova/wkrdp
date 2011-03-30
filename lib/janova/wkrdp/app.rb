94ee6199ee8c1dff1fb05210499c8507df0c2ffb
Anthony Carlos <anthony.carlos@janova.us>
2011-03-30 11:17:20 -0400

    Format assignment section.

--- lib/janova/wkrdp/app.rb
+++ lib/janova/wkrdp/app.rb
@@ -5,20 +5,24 @@ module Janova
       attr_reader :ssh_key
 
       def initialize(*argv)
-        @options = Options.new(argv)
-        @log = Logger.new(STDOUT)
-        @log.level = Logger::WARN
-        @access_key_id = ENV['AMAZON_ACCESS_KEY_ID']
+        @options           = Options.new(argv)
+        @log               = Logger.new(STDOUT)
+        @log.level         = Logger::WARN
+        @access_key_id     = ENV['AMAZON_ACCESS_KEY_ID']
         @secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']
-        @worker_password = ENV['WORKER_PASSWORD']
-        @ssh_key = ENV['BRIANL_SSH_KEY_PATH']
+        @worker_password   = ENV['WORKER_PASSWORD']
+        @ssh_key           = ENV['BRIANL_SSH_KEY_PATH']
+      end
+
+      def validate_environment_variables
         if @access_key_id.nil? || @secret_access_key.nil? || @worker_password.nil? || @ssh_key.nil?
-          puts 'AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY, WORKER_PASSWORD, or BRIANL_SSH_KEY_PATH environment variable(s) not set. Exiting.'
-          exit 1
+          raise ArgumentError, "AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY, WORKER_PASSWORD, or BRIANL_SSH_KEY_PATH environment variable(s) not set. Exiting."
         end
       end
 
       def run
+        validate_environment_variables
+        @options.validate
         ec2 = RightAws::Ec2.new(@access_key_id, @secret_access_key, :logger => @log)
         if @options.list
           list = List.new(ec2, @options)
